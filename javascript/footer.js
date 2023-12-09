05cd004bfec1fab31e84d12d6035d47f7ed8f30f
kumarprashant513 <147999914+kumarprashant513@users.noreply.github.com>
2023-12-09 08:52:26 -0800

    landtwister index.html added

---
+++ javascript/footer.js
@@ -0,0 +1,40 @@
+// document.write(`<div class="row8">
+// <div class="items13">
+//     <div class="box1">
+//     <img src="img/landtwisters logo.jpg" alt="">
+// </div>
+//     <div class="box2">
+
+
+//         <p class="p1">Address</p>
+
+// <p class="p2">Opposite Ansal Golf Link 2, Sector-115, Landra Kharar, Sas Nagar, Chandigarh - 160055</p>
+
+// <p class="p1">Contact No.</p>
+
+// <p class="p2" >+(91) 9115569633</p>
+// <p class="p1">Need Live Support?</p>
+
+// <p class="p2">info@landtwisters.com</p>
+
+
+// <p style="margin-top: 3rem; font-size: 14px;">© 2023 Land Twisters - All rights reserved</p>
+
+   
+// </div>
+// </div>
+// <div class="items13" id="items13">
+//     <p class="p2" id="p2">Our Services</p>
+// <p class="p1" id="p1">Buy</p>
+// <p class="p1" id="p1">Sell</p>
+// <p class="p1" id="p1">Invest</p>
+
+// </div>
+// <div class="items13" id="items13">
+// <p class="p2" id="p2"> Quick Links</p>
+// <p class="p1" id="p1">Commercial / Retail</p>
+// <p class="p1" id="p1">Residential</p>
+// <p class="p1" id="p1">Land</p>
+// <p class="p1" id="p1">Warehouse</p>
+// </div>
+// </div>`)
---
+++ javascript/header.js
@@ -0,0 +1,20 @@
+// document.write(`<div class="row1">
+// <div class="items1">RERA & GMADA Approved</div>
+// <div class="items2">Ph :+91 8288022030 | Email :info@landtwisters.com</div>
+// </div>
+
+
+// <div class="row2">
+// <div class="items3"><img src="img/landtwisters logo.jpg" alt=""></div>
+// <div class="items4">
+//     <li>
+//     <a href="">Home</a>
+//     <a href="">About</a>
+//     <a href="">Our Services</a>
+//     <a href="">Properties</a>
+//     <a href="">Blogs</a>
+//     <a href="">Contact Us</a>
+//     <a href="" class="btn"><i class="fa-solid fa-right-to-bracket"></i> &nbsp;Login</a>
+// </li>
+// </div>
+// </div>`)
